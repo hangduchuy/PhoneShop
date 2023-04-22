bca7d53897ecc19530d968848067858994f0010a
hangduchuy <duchuy246938@gmail.com>
2023-04-22 23:59:15 +0700

    lan6

--- WEBPHONE/Areas/Admin/Controllers/BaoCaoController.cs
+++ WEBPHONE/Areas/Admin/Controllers/BaoCaoController.cs
@@ -20,25 +20,46 @@ namespace WEBPHONE.Areas.Admin.Controllers
         public ActionResult Report()
         {
             var db = new ShopConnectionDB();
-            var data = db.Fetch<dynamic>("SELECT TongTien FROM HoaDon ORDER BY NgayTao DESC");
-            var xValues = ["T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8", "T9", "T10", "T11", "T12"];
+            var data = db.Fetch<dynamic>("SELECT MONTH(NgayTao) AS Thang, SUM(TongTien) AS TongThang FROM HoaDon GROUP BY MONTH(NgayTao) ORDER BY MONTH(NgayTao) ASC");
+            var data2 = db.Fetch<dynamic>("SELECT YEAR(NgayTao) AS Nam, SUM(TongTien) AS TongNam FROM HoaDon GROUP BY YEAR(NgayTao) ORDER BY YEAR(NgayTao) ASC");
 
+            //Tháng
             var chartData = new
-            {
-                labels = "Total",
-                //labels = data.Select(x => x.).ToArray(),
+            {               
+                labels = data.Select(x => x.Thang).ToArray(),
+               
                 datasets = new[]
                 {
-            new
+                    new
+                    {
+                        label = "Tổng tiền của tháng",
+                        data = data.Select(x => x.TongThang).ToArray(),
+                        backgroundColor = "#3e95cd",
+                
+                    }
+                }
+            };
+
+            //Năm
+            var chartData2 = new
             {
-                label = "Total",
-                data = data.Select(x => x.TongTien).ToArray(),
-                backgroundColor = "#3e95cd"
-            }
-        }
+                labels = data2.Select(x => x.Nam).ToArray(),
+
+                datasets = new[]
+                {
+                    new
+                    {
+                        label = "Tổng tiền của Năm",
+                        data = data2.Select(x => x.TongNam).ToArray(),
+                        //backgroundColor = "#3e95cd",
+                        backgroundColor = ["#4e73df", "#1cc88a", "#36b9cc"]
+
+                    }
+                }
             };
 
             ViewBag.ChartData = JsonConvert.SerializeObject(chartData);
+            ViewBag.ChartData2 = JsonConvert.SerializeObject(chartData2);
 
             return View();
         }
